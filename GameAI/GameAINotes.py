ea7bc8470f4cc206147fc851d0e39f7aa94b6055
HardCake4360 <76778281+HardCake4360@users.noreply.github.com>
2025-05-16 15:17:16 +0900

    0516수업내용

---
+++ GameAI/GameAINotes.py
@@ -0,0 +1,11 @@
+#Dynamic NPC Dialog
+"""
+LLAMA 3 사용(5~7B)
+로컬 학습 or Colab 이용
+
++ 
+Fing Tuning
+LORA training colab: 거대 모델을 이용한 추가 모델 학습 -> 파인 튜닝을 통해 특정 도메인에 특화된 데이터 산출
+> LLAMA3 fine tuning using QLoRA
+RAG: DB를 연결시켜 프롬프트에 넣음 - 아마 이거 써봐야할듯
+"""
